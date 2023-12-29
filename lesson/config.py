cf0c2779d39d0b8b791eaf4b488fc6ca034ed74c
aerowskiy <leluhd5@gmail.com>
2023-12-29 15:26:42 +0200

    update config, delete local

--- lesson/config.py
+++ lesson/config.py
@@ -1,5 +1,4 @@
 import os
-import sys
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
 
@@ -7,7 +6,3 @@ FLASK_DEBUG = True
 SQLALCHEMY_DATABASE_URI = f"{os.environ['POSTGRES_NAME']}://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-sys.setdefaultencoding('utf-8')
-os.putenv('LANG', 'en_US.UTF-8')
-os.putenv('LC_ALL', 'en_US.UTF-8')
